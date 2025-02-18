6abe2759c6f750b58733930009919a45f6599129
Moisés Silva <moiss.silva@outlook.com>
2025-02-17 22:08:47 -0300

    Trying web assembly for the first time

---
+++ trying-webassembly/modulo/src/lib.rs
@@ -0,0 +1,21 @@
+use wasm_bindgen::prelude::*;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[wasm_bindgen]
+pub fn texto() -> String {
+	String::from("Bem-vindo ao mundo do WebAssembly")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
